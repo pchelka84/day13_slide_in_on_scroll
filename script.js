1b1b57cec65a2ac4b1f6078a1f5a2cbd53772dcf
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-18 20:10:28 -0500

    Handle slide image in

--- script.js
+++ script.js
@@ -17,8 +17,19 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in');
 
 
-function checkSlide(e) {
- 
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+    // half way through the image 
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2; 
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    // check if the image half shown 
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    // check if the image is not scrolled passed
+    const isNotScrolledPassed = window.scrollY < imageBottom;
+
+    (isHalfShown && isNotScrolledPassed) ? sliderImage.classList.add('active') : sliderImage.classList.remove('active');
+  })
 }
 
 window.addEventListener('scroll', debounce(checkSlide));
