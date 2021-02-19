9616981f1671c9d3c7ae21a526b7264b23a7c72f
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-18 19:52:56 -0500

    Listen for scroll event with debounce

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// To limit the rate at which a function can fire
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
+const sliderImages = document.querySelectorAll('.slide-in');
+
+
+function checkSlide(e) {
+ 
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
